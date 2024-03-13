fe9f0c77c915cac15daeee0564a0b6c931e5feca
Mathias <stadler-mathias@web.de>
2024-03-13 19:11:26 +0100

    finish for today

---
+++ examples/main_anyhow.rs
@@ -0,0 +1,26 @@
+#[allow(unused_imports)]
+use anyhow::{Context, Result};
+#[allow(unused_imports)]
+use std::error::Error;
+#[allow(unused_imports)]
+use std::fmt::{Debug, Display, Formatter};
+
+fn main() {
+    if let Err(err) = try_main() {
+        eprintln!("ERROR: {:#?}", err);
+        err.chain()
+            .skip(1)
+            .try_for_each(|cause| -> Result<(), Box<dyn Error + Send + Sync>> {
+                eprintln!("because: {}", cause);
+                Ok(())
+            }
+            }
+
+        std::process::exit(1);
+    }
+    std::process::exit(0);
+}
+
+fn try_main() {
+    println!("Hello anyhow");
+}
